18564e1a5db52d934512bb6267334621723e70c5
nicolas <zepouet@gmail.com>
2015-12-15 15:11:19 +0100

    Update Applications with new spring boot usecases

---
+++ spring-boot-mysql/src/main/java/hello/controllers/MainController.java
@@ -0,0 +1,17 @@
+package hello.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    public String index() {
+        return "Powered by " +
+                "<a href='http://www.cloudunit.io'>CloudUnit PaaS</a> :)";
+    }
+
+}
