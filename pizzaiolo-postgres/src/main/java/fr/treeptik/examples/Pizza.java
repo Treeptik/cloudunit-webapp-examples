eec9a7506799864d9fd2f87ba2d46172f49c7ea6
nicolas <zepouet@gmail.com>
2016-09-21 16:21:29 +0200

    Correct Pizzaiolo Postgres

---
+++ pizzaiolo-postgres/src/main/java/fr/treeptik/examples/Pizza.java
@@ -0,0 +1,54 @@
+package fr.treeptik.examples;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+
+@Table(name = "pizza")
+public class Pizza {
+	@Id @GeneratedValue private long id;
+	private String name;
+	private double price;
+	
+	/**********************************************************************/
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Pizza pizza = (Pizza) o;
+
+        if (id != pizza.id) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return (int) (id ^ (id >>> 32));
+    }
+}
---
+++ pizzaiolo-postgres/src/main/java/fr/treeptik/examples/PizzaController.java
@@ -0,0 +1,57 @@
+package fr.treeptik.examples;
+
+import java.util.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class PizzaController {
+
+	@Autowired
+	private PizzaDAO pizzaDAO;
+
+	@RequestMapping(method = RequestMethod.GET)
+	public String list(Model model) {
+		List<Pizza> pizzas = pizzaDAO.findAll();
+		model.addAttribute("pizzas", pizzas);
+		model.addAttribute("envProps", listEnvProperties());
+		model.addAttribute("systemProps", listSystemProperties());
+		return "index";
+	}
+
+	private String listSystemProperties() {
+		StringBuilder msg = new StringBuilder();
+		Properties properties = System.getProperties();
+		Enumeration<Object> enumeration = properties.keys();
+		while (enumeration.hasMoreElements()) {
+			msg.append("<tr>");
+			String cle = (String) enumeration.nextElement();
+			String value = properties.getProperty(cle);
+			msg.append("<td>").append(cle).append("</td>");
+			msg.append("<td>").append(value).append("</td>");
+			msg.append("</tr>");
+		}
+		return msg.toString();
+	}
+
+	private String listEnvProperties() {
+		StringBuilder msg = new StringBuilder();
+		Map<String, String> env = System.getenv();
+		Iterator<String> iter = env.keySet().iterator();
+		while (iter.hasNext()) {
+			msg.append("<tr>");
+			String cle = (String)iter.next();
+			String value = env.get(cle);
+			msg.append("<td>").append(cle).append("</td>");
+			msg.append("<td>").append(value).append("</td>");
+			msg.append("</tr>");
+		}
+		return msg.toString();
+	}
+
+}
